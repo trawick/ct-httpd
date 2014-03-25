5e150316f23724044c6c1ba01c9ea8a2bf8e1e95
Jeff Trawick <trawick@gmail.com>
2014-03-25 08:25:42 -0400

    .py cleanup: missing license, PEP 8

--- src/proto1/smoketest.py
+++ src/proto1/smoketest.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import re
 import time
 import urllib2
+
 
 def last_line(logfile):
     with open(logfile, 'rb') as f:
@@ -11,6 +27,7 @@ def last_line(logfile):
         f.seek(-4096, 2)
         last = f.readlines()[-1]
     return last
+
 
 def check_log(logfile, peer_status, proxy_sct_sources):
     time.sleep(1)
@@ -21,6 +38,7 @@ def check_log(logfile, peer_status, proxy_sct_sources):
     assert m
     assert m.group(1) == peer_status
     assert m.group(2) == proxy_sct_sources
+
 
 def test(url, expected_code, echo=False):
     rsp = None
--- src/proto1/statuscgi.py
+++ src/proto1/statuscgi.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
-# import cgi
 import os
 
 print "Content-type: text/html"
 print
 
-# cgi.print_environ()
-
 t = '<table border="1">\n'
-for v in ['HTTPS', 'REMOTE_ADDR', 'REMOTE_PORT', 'SSL_CT_PEER_STATUS', 'SSL_PROTOCOL',
-          'SSL_SESSION_RESUMED']:
+for v in ['HTTPS', 'REMOTE_ADDR', 'REMOTE_PORT', 'SSL_CT_PEER_STATUS',
+          'SSL_PROTOCOL', 'SSL_SESSION_RESUMED']:
     t += '''<tr>
   <td>%s</td><td>%s</td>
 ''' % (v, os.environ.get(v))
 
 t += '</table>\n'
 print t
-
-
