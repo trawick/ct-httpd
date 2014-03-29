6b8c8289902714abc0c61bea7c424656f96a33b9
unknown <trawick@gmail.com>
2014-03-29 11:26:51 -0400

    don't forget to install

--- src/proto1/winbld.cmd
+++ src/proto1/winbld.cmd
@@ -1,10 +1,11 @@
 SETLOCAL
 SET SSL_CT_CMAKE_OPTS=-DCMAKE_INSTALL_PREFIX=%HOME%\PREFIXES\251 -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
-SET BLDDIR=%HOME%\buildirs\ssl_ct
+SET BLDDIR=%HOME%\BUILDDIRS\ssl_ct
 cd %HOME%
 rd /s /q %BLDDIR%
 mkdir %BLDDIR%
 cd %BLDDIR%
 cmake %SSL_CT_CMAKE_OPTS% %HOME%\git\ct-httpd\src\proto1
 nmake
+nmake install
