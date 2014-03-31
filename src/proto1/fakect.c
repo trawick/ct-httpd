927732b2b1b4b06bf94f8f85365a1d3f75a6783f
unknown <trawick@gmail.com>
2014-03-31 11:41:46 -0400

    placeholder for ct.exe on Windows

---
+++ src/proto1/fakect.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+int main(void)
+{
+    system("copy %HOME%\\AUTO_localhost_8888_-.sct %TEMP%\\d9863e419fc90a7faab64952d3d71a389734f82214fb3817cb5c88be81c2f1cb\\");
+    return 0;
+}
