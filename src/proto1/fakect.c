e885c6ac9c90ca5c2fe54a380752c7d0775b4fc0
unknown <trawick@gmail.com>
2014-04-01 10:20:09 -0400

    update mtime so that the SCT doesn't have to be re-fetched (ha!) over and over

--- src/proto1/fakect.c
+++ src/proto1/fakect.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 
+#define BASENAME "AUTO_localhost_8888_-.sct"
+#define DIRNAME  "d9863e419fc90a7faab64952d3d71a389734f82214fb3817cb5c88be81c2f1cb"
+
 int main(void)
 {
-    system("copy %HOME%\\AUTO_localhost_8888_-.sct %TEMP%\\d9863e419fc90a7faab64952d3d71a389734f82214fb3817cb5c88be81c2f1cb\\");
+    system("copy %HOME%\\" BASENAME " %TEMP%\\" DIRNAME "\\");
+    system("powershell (ls %TEMP%\\" DIRNAME "\\" BASENAME ").LastWriteTime = Get-Date");
     return 0;
 }
