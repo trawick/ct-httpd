9dbff18054473d1e28df639776e03ee6d1fa0c1b
unknown <trawick@gmail.com>
2014-04-01 14:16:16 -0400

    waste a little more time avoiding getting the certificate-transparency tools to build on Windows...  the PowerShell one-liner failed without a console, so an old hack is used to update the timestamp

--- src/proto1/fakect.c
+++ src/proto1/fakect.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #define BASENAME "AUTO_localhost_8888_-.sct"
@@ -5,7 +6,26 @@
 
 int main(void)
 {
+    char *destdir;
+
     system("copy %HOME%\\" BASENAME " %TEMP%\\" DIRNAME "\\");
-    system("powershell (ls %TEMP%\\" DIRNAME "\\" BASENAME ").LastWriteTime = Get-Date");
+    destdir = getenv("TEMP");
+    if (!destdir) {
+        fprintf(stderr, "TEMP isn't set\n");
+        return 1;
+    }
+
+    /* "touch" idiom on Windows 7 requires setting the current directory to the
+     * one with the file to "touch"
+     */
+    destdir = malloc(strlen(getenv("TEMP")) + strlen("\\" DIRNAME) + 1);
+    strcpy(destdir, getenv("TEMP"));
+    strcat(destdir, "\\" DIRNAME);
+    if (chdir(destdir) != 0) {
+        fprintf(stderr, "couldn't change to %s\n", destdir);
+        return 1;
+    }
+
+    system("copy /b " BASENAME " +,,");
     return 0;
 }
